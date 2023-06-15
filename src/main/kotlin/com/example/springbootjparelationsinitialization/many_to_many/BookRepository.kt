1543e89a5904851f6fb99a96c8fe1fb93a959fbf
Javi <javi@jsrois.net>
2023-06-15 11:52:04 +0200

    books by author

--- src/main/kotlin/com/example/springbootjparelationsinitialization/many_to_many/BookRepository.kt
+++ src/main/kotlin/com/example/springbootjparelationsinitialization/many_to_many/BookRepository.kt
@@ -1,7 +1,13 @@
 package com.example.springbootjparelationsinitialization.many_to_many
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
-interface BookRepository : JpaRepository<Book, Long>
+interface BookRepository : JpaRepository<Book, Long> {
+
+    @Query("select b from Book b join b.authors a where a.id = :authorId")
+    fun findBooksFromAuthorId(@Param("authorId") authorId: Long): List<Book>
+}
--- src/main/kotlin/com/example/springbootjparelationsinitialization/many_to_many/MainController.kt
+++ src/main/kotlin/com/example/springbootjparelationsinitialization/many_to_many/MainController.kt
@@ -1,7 +1,12 @@
 package com.example.springbootjparelationsinitialization.many_to_many
 
+import org.hibernate.annotations.NotFound
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+import kotlin.jvm.optionals.getOrElse
 
 @RestController
 class MainController(private val authorRepository: AuthorRepository, private val bookRepository: BookRepository) {
@@ -9,6 +14,17 @@ class MainController(private val authorRepository: AuthorRepository, private val
     fun allAuthors(): List<AuthorResponse> = authorRepository
             .findAll()
             .map(AuthorResponse::from)
+
+    @GetMapping("/api/authors/{authorId}/books")
+    fun booksByAuthor(@PathVariable authorId: Long): List<Book> {
+        return authorRepository.findById(authorId)
+                .getOrElse { null }
+                ?.let {
+                    bookRepository.findBooksFromAuthorId(it.id)
+                }
+                ?: throw ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Unable to find author with id $authorId")
+    }
 
     @GetMapping("/api/books")
     fun allBooks(): List<BookResponse> =
